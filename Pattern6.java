5edb947ba6f18f5f7fb136aea8903f75579a0a93
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-01-27 12:43:49 +0530

    Create Pattern6.java

---
+++ Pattern6.java
@@ -0,0 +1,49 @@
+// Enter n
+// 10
+//  * 
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+//  * * * * * *
+//  * * * * * * *
+//  * * * * * * * * 
+//  * * * * * * * * *
+//  * * * * * * * * * *
+//  * * * * * * * * *
+//  * * * * * * * * 
+//  * * * * * * *
+//  * * * * * *
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+ 
+
+import java.util.Scanner;
+
+public class Pattern6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        sc.close();
+       for(int i=1;i<=n;i++){
+           System.out.print(" ");
+           for(int j=1; j<=i;j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+       for(int i=1; i<=n;i++){
+           System.out.print(" ");
+           for(int j=1; j<=(n-i);j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+    }
+
+}
