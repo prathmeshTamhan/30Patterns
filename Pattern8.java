8de6e760c3806d82e72d9d375e8bd0c0879c4146
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-01-29 11:20:43 +0530

    Create Pattern8.java

---
+++ Pattern8.java
@@ -0,0 +1,43 @@
+// Enter n
+// 5
+//     * 
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
+
+import java.util.Scanner;
+
+public class Pattern8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int space = n; space >= i; space--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
