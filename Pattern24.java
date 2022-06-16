48465a6e939ab77956e817060e0e8f40958b3981
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-06-16 22:28:05 +0530

    Create Pattern24.java

---
+++ Pattern24.java
@@ -0,0 +1,26 @@
+// Enter n
+// 5
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+
+import java.util.Scanner;
+public class Pattern24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        sc.close();
+         int alphabet = 65;
+        // int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (alphabet +i-1));
+            }
+            System.out.println();
+        }
+    }
+}
